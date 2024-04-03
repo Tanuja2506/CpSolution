22dfaf63c0576629e331b2446e3d5a41336b5125
Tanuja2506 <121607300+Tanuja2506@users.noreply.github.com>
2024-04-03 20:39:46 +0530

    Create Trip.c

---
+++ Trip.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+    int n, i;
+    float a[100], sum = 0, avg;
+
+    printf("Enter the numbers of students: ");
+    scanf("%d", &n);
+    printf("Enter Money spent by each student");
+    for (i = 0; i < n; ++i) {
+        scanf("%f", &a[i]);
+        sum += a[i];
+    }
+
+    avg = sum / n;
+    printf("\nAverage = %.2f", avg);
+    float diff=0;
+    for(i=0;i<n;i++){
+        if(a[i]>avg)
+          diff=diff+(a[i]-avg);
+    }
+   printf("\n Difference is:%f",diff);
+    return 0;
+}
